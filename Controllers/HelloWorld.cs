dbae54a92a53a74c88338fb49e660e69925248e2
Esaú García <Esa Garca@DESKTOP-HIT0809>
2025-03-18 22:36:46 -0600

    Agregando cambios

---
+++ Controllers/HelloWorld.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace EjemProyect.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorld : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+
+        [HttpGet("GetSaludo/{nombre}")]
+        public string Get(string nombre) 
+        {
+            return "Hola " + nombre;
+        }
+
+        private struct user
+        {
+            public string nombre;
+            public int id;
+            public string edad;
+        }
+
+        [HttpGet("GetUsuario/{id}/{nombre}/{edad}")]
+        public string Get(string edad, string nombre, int id)
+        {
+            user usuario = new user();
+            usuario.edad = edad;
+            usuario.id = id;
+            usuario.nombre = nombre;
+
+            string respuesta = JsonConvert.SerializeObject(usuario);
+            return respuesta;
+        }
+
+        public class UsuarioP
+        {
+            public int id { get; set;  }
+            public string name { get; set; }
+        }
+         
+        [HttpPost("PostUser")] 
+        public string Post(UsuarioP usuario) 
+        {
+            return JsonConvert.SerializeObject(usuario);
+        }
+
+        [HttpDelete("DelUsuario")]
+        public string Delete(int id_Usuario) 
+        {
+            return "Se eliminó correctamente el Usuario";
+        }
+    }
+}
