7f83e195e29c611477b497a89d9ebdc2606b692a
Esaú García <Esa Garca@DESKTOP-HIT0809>
2025-03-18 22:36:46 -0600

    ActualizacionAPIAgregando cambios

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using EjemProyect.Controllers;
+
 namespace EjemProyect
 {
     public class Program
@@ -12,6 +14,7 @@ namespace EjemProyect
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<ServicesDemo>();
 
             var app = builder.Build();
 
