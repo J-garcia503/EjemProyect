229d0a0aa5508100f637a41fbf9c5ba0368c0056
Esaú García <Esa Garca@DESKTOP-HIT0809>
2025-03-28 20:16:31 -0600

    ActualizacionAPI

---
+++ Profiles/ClientProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EjemProyect.Models;
+
+namespace EjemProyect.Profiles
+{
+    public class ClientProfile : Profile
+    {
+        public ClientProfile() 
+        {
+            CreateMap<ClienteRequest, Cliente>()
+                .ForMember(d => d.Name, o => o.MapFrom(s => s.Nombre))
+                .ForMember(d => d.Email, o => o.MapFrom(s => s.Correo));
+        }
+    }
+}
